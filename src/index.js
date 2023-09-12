f183599e121e0e610162c51a82793f0d229e8ea3
Geilson Maciel <geilsonm2@hotmail.com>
2023-09-11 23:10:48 -0300

    Primeiro Desafio Finalizado utilizando if e else if

--- src/index.js
+++ src/index.js
@@ -1,1 +1,37 @@
-console.log('testando..')
+let nomeHeroi = 'Soneca.q'
+let quantidadeXP = 9000
+
+
+if (quantidadeXP <= 1000){
+    nivelHeroi = "Ferro"
+    console.log("O Herói de nome " + nomeHeroi + " está no nível de " + nivelHeroi)
+
+}else if(quantidadeXP >= 1001 && quantidadeXP <= 2000){
+    nivelHeroi = "Bronze"
+    console.log("O Herói de nome " + nomeHeroi + " está no nível de " + nivelHeroi)
+
+}else if(quantidadeXP >= 2001 && quantidadeXP <= 5000){
+    nivelHeroi = "Prata"
+    console.log("O Herói de nome " + nomeHeroi + " está no nível de " + nivelHeroi)
+
+}else if(quantidadeXP >= 6001 && quantidadeXP <= 7000){
+    nivelHeroi = "Ouro"
+    console.log("O Herói de nome " + nomeHeroi + " está no nível de " + nivelHeroi)
+
+}else if(quantidadeXP >= 7001 && quantidadeXP <= 8000){
+    nivelHeroi = "Platina"
+    console.log("O Herói de nome " + nomeHeroi + " está no nível de " + nivelHeroi)
+
+}else if(quantidadeXP >= 8001 && quantidadeXP <= 9000){
+    nivelHeroi = "Ascendente"
+    console.log("O Herói de nome " + nomeHeroi + " está no nível de " + nivelHeroi)
+    
+}else if(quantidadeXP >= 9001 && quantidadeXP <= 10000){
+    nivelHeroi = "Imortal"
+    console.log("O Herói de nome " + nomeHeroi + " está no nível de " + nivelHeroi)
+
+}else if(quantidadeXP >= 10000){
+    nivelHeroi = "Radiante"
+    console.log("O Herói de nome " + nomeHeroi + " está no nível de " + nivelHeroi)
+}
+
